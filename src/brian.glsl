a23ce769d4d462949c4e94244c282ba7590b9e77
David Valachovic <David.E.Valachovic@gmail.com>
2019-02-03 18:31:29 -0600

    colors and fading

--- src/brian.glsl
+++ src/brian.glsl
@@ -2,32 +2,57 @@ precision mediump float;
 
 uniform sampler2D previousState;
 
+int wasPixelAlive(float pixel) {
+  return pixel == 0.0
+    ? 1
+    : 0;
+}
+
+int wasPixelDying(float pixel) {
+  return pixel == 0.5
+    ? 1
+    : 0;
+}
+
+int wasPixelDead(float pixel) {
+  return pixel == 1.0
+    ? 1
+    : 0;
+}
+
 int wasAlive(vec2 coord) {
-  if (coord.x < 0.0 || 1024.0 < coord.x || coord.y < 0.0 || 1024.0 < coord.y) return 0;
+  float size = 1024.0;
 
-  vec4 px = texture2D(previousState, coord / 1024.0);
+  if (
+    coord.x < 0.0 || 
+    coord.x > size || 
+    coord.y < 0.0 || 
+    coord.y > size
+  ) return 0;
 
-  return px.r < 0.1 ? 1 : 0;
+  vec4 px = texture2D(previousState, coord / size);
+
+  return wasPixelAlive(px.r);
 }
 
 void main(void) {
-  vec2 coord = vec2(gl_FragCoord);
+  vec2 coord = gl_FragCoord.xy;
 
   int aliveNeighbors =
-    wasAlive(coord+vec2(-1.,-1.)) +
-    wasAlive(coord+vec2(-1.,0.)) +
-    wasAlive(coord+vec2(-1.,1.)) +
-    wasAlive(coord+vec2(0.,-1.)) +
-    wasAlive(coord+vec2(0.,1.)) +
-    wasAlive(coord+vec2(1.,-1.)) +
-    wasAlive(coord+vec2(1.,0.)) +
-    wasAlive(coord+vec2(1.,1.));
+    wasAlive(coord + vec2(-1.0, -1.0)) +
+    wasAlive(coord + vec2(-1.0, 0.0)) +
+    wasAlive(coord + vec2(-1.0, 1.0)) +
+    wasAlive(coord + vec2(0.0, -1.0)) +
+    wasAlive(coord + vec2(0.0, 1.0)) +
+    wasAlive(coord + vec2(1.0, -1.0)) +
+    wasAlive(coord + vec2(1.0, 0.0)) +
+    wasAlive(coord + vec2(1.0, 1.0));
 
   bool nowAlive = wasAlive(coord) == 1
-    ? 2 <= aliveNeighbors && aliveNeighbors <= 3
-    : 3 == aliveNeighbors;
+    ? aliveNeighbors == 2 || aliveNeighbors == 3
+    : aliveNeighbors == 3;
   
   gl_FragColor = nowAlive
-    ? vec4(0.,0.,0.,1.) 
-    : vec4(1.,1.,1.,1.);
+    ? vec4(0.0, 0.0, 0.0, 1.0) 
+    : vec4(1.0, 1.0, 1.0, 1.0);
 }
--- src/conway.glsl
+++ src/conway.glsl
@@ -2,32 +2,48 @@ precision mediump float;
 
 uniform sampler2D previousState;
 
+float size = 1024.0;
+float fadeRate = 0.003;
+
+int wasPixelAlive(float pixel) {
+  return pixel == 1.0
+    ? 1
+    : 0;
+}
+
 int wasAlive(vec2 coord) {
-  if (coord.x < 0.0 || 1024.0 < coord.x || coord.y < 0.0 || 1024.0 < coord.y) return 0;
+  if (
+    coord.x < 0.0 || 
+    coord.x > size || 
+    coord.y < 0.0 || 
+    coord.y > size
+  ) return 0;
 
-  vec4 px = texture2D(previousState, coord / 1024.0);
+  vec4 px = texture2D(previousState, coord / size);
 
-  return px.r < 0.1 ? 1 : 0;
+  return wasPixelAlive(px.r);
 }
 
 void main(void) {
-  vec2 coord = vec2(gl_FragCoord);
+  vec2 coord = gl_FragCoord.xy;
+  vec2 coordNormalized = coord / size;
+  vec4 px = texture2D(previousState, coordNormalized);
 
   int aliveNeighbors =
-    wasAlive(coord+vec2(-1.,-1.)) +
-    wasAlive(coord+vec2(-1.,0.)) +
-    wasAlive(coord+vec2(-1.,1.)) +
-    wasAlive(coord+vec2(0.,-1.)) +
-    wasAlive(coord+vec2(0.,1.)) +
-    wasAlive(coord+vec2(1.,-1.)) +
-    wasAlive(coord+vec2(1.,0.)) +
-    wasAlive(coord+vec2(1.,1.));
+    wasAlive(coord + vec2(-1.0, -1.0)) +
+    wasAlive(coord + vec2(-1.0, 0.0)) +
+    wasAlive(coord + vec2(-1.0, 1.0)) +
+    wasAlive(coord + vec2(0.0, -1.0)) +
+    wasAlive(coord + vec2(0.0, 1.0)) +
+    wasAlive(coord + vec2(1.0, -1.0)) +
+    wasAlive(coord + vec2(1.0, 0.0)) +
+    wasAlive(coord + vec2(1.0, 1.0));
 
   bool nowAlive = wasAlive(coord) == 1
-    ? 2 <= aliveNeighbors && aliveNeighbors <= 3
-    : 3 == aliveNeighbors;
+    ? aliveNeighbors == 2 || aliveNeighbors == 3
+    : aliveNeighbors == 3;
   
   gl_FragColor = nowAlive
-    ? vec4(0.,0.,0.,1.) 
-    : vec4(1.,1.,1.,1.);
+    ? vec4(1.0, coordNormalized.x / 1.0, coordNormalized.y / 1.0, 1.0) 
+    : px - vec4(fadeRate, fadeRate, fadeRate, 1.0);
 }
